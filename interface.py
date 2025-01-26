7e784809e7c58da93c83f5deb1f870a928e10a01
EsauRoiDesBarbus <etiennelebertin@gmail.com>
2025-01-26 15:02:48 +0100

    switched to new eclipseCpp

--- interface.py
+++ interface.py
@@ -1,9 +1,7 @@
-from eclipseCpp import BattleBuilder
+from eclipseCpp_interface import *
 import re
 
 def solveBattle (battle_info):
-
-    battle_builder = BattleBuilder ()
     regex1 = re.search("(.*)(vs|VS|Vs|vS)(.*)" , battle_info)
     sides = [regex1[1], regex1[3]]
 
@@ -11,30 +9,41 @@ def solveBattle (battle_info):
     for i in range (14):
         ship_re += r" +(\d+)"
 
-    ship_side = "ATT" # first list is attack
+    # type conversion (because of legacy nomenclarure)
+    type_dico = {
+        "int": "INT",
+        "cru": "CRU",
+        "dre": "DRE",
+        "sba": "SBA",
+        "npc": "INT"
+    }
+
+    attacker_ships = []
+    defender_ships = []
+    player_ships = attacker_ships
     for side in sides:
         ships = side.split('+')
         for ship in ships:
-            print (ship)
+            #TODO npc
             
             regex = re.search(ship_re, ship) #number type init hull comp shield weapons
-            print (regex)
             
             canons   = [int(regex[ 7]), int(regex[ 8]), int(regex[ 9]), int(regex[10]), int(regex[11])]
             missiles = [int(regex[12]), int(regex[13]), int(regex[14]), int(regex[15]), int(regex[16])]
 
-            battle_builder.addShip (ship_side, int(regex[1]), "INT", int(regex[3]), int(regex[4]), int(regex[5]), int(regex[6]), canons, missiles)
+            player_ships.append (Ship (int(regex[1]), type_dico[regex[2]], int(regex[3]), int(regex[4]), int(regex[5]), int(regex[6]), canons, missiles))
 
-        ship_side = "DEF" #switch to def for the second list
+        player_ships = defender_ships #switch to def for the second list
     # call C++ lib to solve battle
-    battle_builder.solveBattle (timeout=60)
-    result = battle_builder.getResult ()
+    battle = Battle (attacker_ships, BattleModifier(), defender_ships, BattleModifier())
+    print(battle.solveBattle (timeout=60))
+    result = battle.getResult ()
 
     # TODO? change frontend to take result directly
     dico = {
-        "winChance": result["attacker_win_chance"],
-        "attackShipsStillAlive" : result["attacker_ship_survival_chance"],
-        "defenseShipsStillAlive": result["defender_ship_survival_chance"]
+        "winChance": float(result["attacker_win_chance"]),
+        "attackShipsStillAlive" : [[float(i) for i in chance] for chance in result["attacker_ship_survival_chance"]],
+        "defenseShipsStillAlive": [[float(i) for i in chance] for chance in result["defender_ship_survival_chance"]]
     }
 
     return dico
